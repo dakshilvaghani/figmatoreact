9bf0f4e7866ab09415a2af35855518475f524fb3
dakshilvaghani <119387136+dakshilvaghani@users.noreply.github.com>
2024-12-02 20:30:30 +0530

    removing unnecessary routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { useRoutes } from "react-router-dom";
-import Home from "../src/pages/Home";
-import NotFound from "../src/pages/NotFound";
 import MendlesonLandingpageDesign from "../src/pages/MendlesonLandingpageDesign";
 
 const ProjectRoutes = () => {
   let element = useRoutes([
-    { path: "/", element: <Home /> },
-    { path: "*", element: <NotFound /> },
     {
-      path: "mendlesonlandingpagedesign",
+      path: "/",
       element: <MendlesonLandingpageDesign />,
     },
   ]);
